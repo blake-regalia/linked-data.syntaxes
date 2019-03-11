38b887c39f03149865b48a110e71f45b2eabdf34
Blake Regalia <blake.regalia@gmail.com>
2019-03-11 15:56:22 -0700

    fix(travis): branch name typo asset => assets

--- push-assets.sh
+++ push-assets.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 PACKAGE_VERSION="$(npm info linked-data.syntaxes version)"
-git checkout --orphan asset
+git checkout --orphan assets
 git reset --hard
 git rm --cached -r .
 git add build/sublime/LinkedData.sublime-package
 git commit -m "v${PACKAGE_VERSION}"
 git remote add github-asset "https://blake-regalia:${GITHUB_OAUTH_TOKEN}@github.com/blake-regalia/linked-data.syntaxes.git"
-git push github-asset asset
+git push github-asset assets
